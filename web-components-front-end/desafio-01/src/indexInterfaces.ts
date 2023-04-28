3869b5ec3692cf89e19c3e8d01dd9db4825bdf4f
Jonathan Brito <devjonathanbrito@gmail.com>
2023-04-28 12:31:58 -0300

    Interfaces em typscript

---
+++ web-components-front-end/desafio-01/src/indexInterfaces.ts
@@ -0,0 +1,21 @@
+// Interfaces (type x interface)
+
+type robot = {
+    id: number;
+    name: string;
+}
+
+const bot: robot={
+    id:1,
+    name: "Jonathan Junior",
+};
+
+interface robot2 {
+    id: number;
+    name: string;
+}
+
+const bot1: robot2 = {
+    id: 1,
+    name: "mensagem",
+};
