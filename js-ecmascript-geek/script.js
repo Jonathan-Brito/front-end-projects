27ca8cc1df8f73d0189ac29593e1c9c0fb875c9c
Jonathan Brito <devjonathanbrito@gmail.com>
2023-05-02 15:41:57 -0300

    javascript e ecmascript functions

--- js-ecmascript-geek/script.js
+++ js-ecmascript-geek/script.js
@@ -9,3 +9,19 @@ function perguntar(){
 
     alert("Olá " + nome);
 }
+
+function mudar_texto(){
+    var h1 = document.getElementsByTagName("h1");
+
+    if(h1[0].innerText == "Brito University"){
+        h1[0].innerText = "Evolua o seu lado geek!";
+    } else{
+        h1[0].innerText = "Brito University";
+    }
+}
+
+function incrementar(){
+    var p1 = document.getElementById("p1");
+
+    p1.innerText = parseInt(p1.innerText) + 1;
+}
